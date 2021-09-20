6cfcf7fd8608c6d0e030457d4c2c5a76e1e34cc8
wmvoon <79824380+wmvoon@users.noreply.github.com>
2021-09-20 14:34:16 +0800

    Create quickSort_error_v2.cpp

---
+++ quickSort_error_v2.cpp
@@ -0,0 +1,142 @@
+#include <iostream>
+#include <stdlib.h>
+#include <chrono>
+#include <fstream>
+#include <time.h>
+#include <pthread.h>
+
+using namespace std;
+using namespace std::chrono;
+
+#define max_threads 4
+
+struct Array
+{
+    int seed;
+    int *arr;
+    int start;
+    int end;
+};
+
+// A utility function to swap two elements
+void swap(Array *a, Array *b)
+{
+    Array t = *a;
+    *a = *b;
+    *b = t;
+}
+ 
+/* This function takes last element as pivot, places
+   the pivot element at its correct position in sorted
+    array, and places all smaller (smaller than pivot)
+   to left of pivot and all greater elements to right
+   of pivot */
+int partition (Array arr[], int low, int high)
+{
+    Array pivot = arr[high];    // pivot
+    int i = (low - 1);  // Index of smaller element
+ 
+    for (int j = low; j <= high- 1; j++)
+    {
+        // If current element is smaller than or
+        // equal to pivot
+        if (arr[j] <= pivot)
+        {
+            i++;    // increment index of smaller element
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    swap(&arr[i + 1], &arr[high]);
+    return (i + 1);
+}
+
+void quickSort(Array arr[], int low, int high)
+{
+    if (low < high)
+    {
+        /* pi is partitioning index, arr[p] is now
+           at right place */
+        int pi = partition(arr, low, high);
+ 
+        // Separately sort elements before
+        // partition and after partition
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+} 
+
+/* The main function that implements QuickSort
+ arr[] --> Array to be sorted,
+  low  --> Starting index,
+  high  --> Ending index */
+void *qSort(void *args)
+{   
+    Array *arr = ((struct Array *)args);
+    int arr;
+    int low = 0;
+    int high = (sizeof(arr)/sizeof(arr[0])) - 1;
+    for (int i = arr->start; i < arr->end; i++)
+    {
+        if (low < high)
+        {
+            /* pi is partitioning index, arr[p] is now
+            at right place */
+            int pi = partition(arr, low, high);
+ 
+            // Separately sort elements before
+            // partition and after partition
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+    }
+    return NULL;
+}
+ 
+// Driver program to test above functions
+int main()
+{
+    pthread_t threads[max_threads];
+    int i;
+    int *arr;
+    unsigned long size = 10000L;
+
+    arr = (int *)malloc(size * sizeof(int *));
+  
+    //start time 
+    auto start = high_resolution_clock::now();
+   
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rand() % 10000;
+    }
+
+    //spliting the program to each thread to achieve parallelism
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int p = pthread_create(&threads[i], NULL, qSort, (void*)(arr, 0, n-1));
+
+    //joining all the completed task in each threads
+    for (int i = 0; i < max_threads; i++)
+    {
+        pthread_join(threads[i], NULL);
+    }
+
+    //int n = sizeof(arr)/sizeof(arr[0]);
+    //quickSort(arr, 0, n-1);
+    //printing array
+    printf("Sorted array:\n");
+    for (int i=0; i < size; i++)
+    {  
+        printf("%d ", arr[i]);
+    }
+
+    //stop time
+    auto stop = high_resolution_clock::now();
+    //gets the start and stop time and subtracts it to get the amount of time spent processing.
+    auto duration = duration_cast<microseconds>(stop - start);
+    auto duration_s = duration_cast<milliseconds>(stop - start);
+    //convert milliseconds to seconds 
+    double secs = double(duration_s.count())/1000;
+
+    cout << "\nTime taken to run: " << duration.count() << " microseconds (" << secs << " seconds)"<< endl;
+
+}
